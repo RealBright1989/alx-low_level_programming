d9d8fe21e08d18f746dffa8445c0d94c1e5b19e3
RealBright1989 <brightokon444@gmail.com>
2023-08-17 11:13:08 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,24 +3,24 @@
 /**
  * punct - used to add puctuations
  * @row: current iteration of row
- * @col: currentn iteration fo col
+ * @col: current iteration of col
  *
- * Return: void
+ * Return: vold
  */
 void punct(int row, int col)
 {
-        int cur;
-        int nextm;
+	int cur;
+	int nextm;
 
-        cur = col;
-        nextm = row * (cur + 1);
-        if (col != 9)
-        {
-                _putchar(',');
-                _putchar(32); /* space */
-                if (nextm < 10)
-                        _putchar(32);
-        }
+	cur = col;
+	nextm = row * (cur + 1);
+	if (col != 9)
+	{
+		_putchar(',');
+		_putchar(32); /* space */
+		if (nextm < 10)
+			_putchar(32);
+	}
 }
 /**
  * times_table - prints the 9 times table, starting with 0.
@@ -29,33 +29,32 @@ void punct(int row, int col)
  */
 void times_table(void)
 {
-        int row;
-        int col;
+	int row;
+	int col;
 
-        row = 0;
-        col = 0;
-        while (row <= 9)
-        {
-                while (col <= 9)
-                {
-                        int mult;
+	row = 0;
+	col = 0;
+	while (row <= 9)
+	{
+		while (col <= 9)
+		{
+			int mult;
 
-                        mult = row * col;
-                        if (mult < 10)
+			mult = row * col;
+			if (mult < 10)
 			{
-                                _putchar('0' + mult);
-                                punct(row, col);
-                        }
-                        else
-                        {
-                                _putchar('0' + (mult / 10));
-                                _putchar('0' + (mult % 10));
-                                punct(row, col);
-                        }
-                        col++;
-                }
-                _putchar('\n');
-                row++;
-                col = 0;
-        }
+				_putchar('0' + mult);
+				punct(row, col);
+			}
+			else
+			{
+				_putchar('0' + (mult / 10));
+				_putchar('0' + (mult % 10));
+			}
+			col++;
+		}
+		_putchar('\n');
+		row++;
+		col = 0;
+	}
 }
