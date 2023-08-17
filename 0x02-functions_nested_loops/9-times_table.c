653189d9ff4157c3507abdc1d668e42d1fe94bf6
RealBright1989 <brightokon444@gmail.com>
2023-08-17 07:33:56 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,62 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * punct - used to add puctuations
+ * @row: current iteration of row
+ * @col: currentn iteration fo col
+ *
+ * Return: void
+ */
+void punct(int row, int col)
+{
+        int cur;
+        int nextm;
+
+        cur = col;
+        nextm = row * (cur + 1);
+        if (col != 9)
+        {
+                _putchar(',');
+                _putchar(32); /* space */
+                if (nextm < 10)
+                        _putchar(32);
+        }
+}
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: void
+ */
 void times_table(void)
 {
-	int i, j, result;
+        int row;
+        int col;
 
-	for (i = 0 ; i < 10 ; i++)
-	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			result = i * j;
-			printf("%2d,", result);
-		}
-		printf("\n");
-	}
+        row = 0;
+        col = 0;
+        while (row <= 9)
+        {
+                while (col <= 9)
+                {
+                        int mult;
+
+                        mult = row * col;
+                        if (mult < 10)
+			{
+                                _putchar('0' + mult);
+                                punct(row, col);
+                        }
+                        else
+                        {
+                                _putchar('0' + (mult / 10));
+                                _putchar('0' + (mult % 10));
+                                punct(row, col);
+                        }
+                        col++;
+                }
+                _putchar('\n');
+                row++;
+                col = 0;
+        }
 }
